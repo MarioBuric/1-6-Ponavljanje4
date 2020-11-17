52a6b59ccd465ba4f9d92350d220a96f783fba29
MarioBuric <55134593+MarioBuric@users.noreply.github.com>
2020-11-17 10:24:05 +0100

    Add files via upload

---
+++ buricMarioPonavljanje4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buricMarioPonavljanje4
+{
+    
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.Write("Unesi cijenu proizvoda: ");
+            string itemPrice = Console.ReadLine();
+            Console.Write("Unesi PDV (u postotku): ");
+            string taxRate = Console.ReadLine();
+
+           
+
+            Console.WriteLine();
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ buricMarioPonavljanje4/TaxCalculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buricMarioPonavljanje4
+{
+    class TaxCalculator
+    {
+        private static decimal _cijenaproizvoda;
+        private static decimal _postotakPDV;
+        private static decimal _ukupnacijena;
+
+        public TaxCalculator(string inputcijenaproizvoda, string inputpostotakPDV)
+        {
+            _cijenaproizvoda = Decimal.Parse(inputcijenaproizvoda);
+            _postotakPDV = Decimal.Parse(inputpostotakPDV) / 100;
+        }
+
+        public void CalculateTotalPrice()
+        {
+            _ukupnacijena = _cijenaproizvoda + (_cijenaproizvoda * _postotakPDV);
+        }
+
+        public void GetTotalMsg()
+        {
+         Console.WriteLine("Cijena proizvoda je {0:C} i ukupna cijena je {1:C} na postotku PDV-a od {2:p0}", _cijenaproizvoda, _ukupnacijena, _postotakPDV);
+        }
+    }
+}
